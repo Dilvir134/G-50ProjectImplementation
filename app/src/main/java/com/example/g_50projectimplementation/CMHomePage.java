e48b410c3051bc73bb95d261c34439e56cdee741
Dilvir134 <dilvir134@gmail.com>
2024-11-23 14:12:51 -0500

    initial commit

---
+++ app/src/main/java/com/example/g_50projectimplementation/CMHomePage.java
@@ -0,0 +1,36 @@
+package com.example.g_50projectimplementation;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CMHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_cm_homepage);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void goToSignInPage(View view) {
+        Intent intent = new Intent(CMHomePage.this, SignIn.class);
+        startActivity(intent);
+    }
+    public void goToGoogle(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://google.com"));
+        startActivity(intent);
+    }
+}
