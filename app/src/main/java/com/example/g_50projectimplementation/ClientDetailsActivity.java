57aab79e69326aae0668cdf0ed0bc7dcbe9e675a
britto <jose@britto.tech>
2024-12-06 09:07:04 -0500

    Client details looks pretty

--- app/src/main/java/com/example/g_50projectimplementation/ClientDetailsActivity.java
+++ app/src/main/java/com/example/g_50projectimplementation/ClientDetailsActivity.java
@@ -1,9 +1,18 @@
 package com.example.g_50projectimplementation;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.example.g_50projectimplementation.database.AppDatabase;
 import com.example.g_50projectimplementation.database.entity.Client;
@@ -11,17 +20,41 @@ import com.example.g_50projectimplementation.database.entity.Client;
 public class ClientDetailsActivity extends AppCompatActivity {
 
     private AppDatabase db;
-    private TextView clientNameTextView, clientLocationTextView, clientContactTextView;
-
+    private TextView clientNameTextView, clientLocationTextView;
+    private TextView contactName, contactPhone;
+    private ImageView clientLogo;
+    private Button btnCall;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_client_details);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        Util.fixStatusBarColorLight(getWindow(), this);
 
         // Initialize views
         clientNameTextView = findViewById(R.id.clientNameTextView);
         clientLocationTextView = findViewById(R.id.clientLocationTextView);
-        clientContactTextView = findViewById(R.id.clientContactTextView);
+        contactName = findViewById(R.id.contactName);
+        contactPhone = findViewById(R.id.contactPhone);
+        clientLogo  = findViewById(R.id.clientLogo);
+        btnCall = findViewById(R.id.btnCall);
+
+        btnCall.setOnClickListener(l -> {
+            String phone = contactPhone.getText().toString().trim();
+            Log.d("PHONE", phone);
+            if(!phone.isEmpty()) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + phone));
+                startActivity(intent);
+            }
+        });
 
         // Get the passed clientId
         int clientId = getIntent().getIntExtra("CLIENT_ID", -1);
@@ -35,7 +68,11 @@ public class ClientDetailsActivity extends AppCompatActivity {
                     if (client != null) {
                         clientNameTextView.setText(client.getName());
                         clientLocationTextView.setText(client.getLocation());
-                        clientContactTextView.setText(client.getContactName()); // Assuming a field 'contactInfo'
+                        contactName.setText(client.getContactName());
+                        contactPhone.setText(client.getContactNumber());
+                        if(client.getLogoUrl() != null) {
+                            clientLogo.setImageURI(Uri.parse(client.getLogoUrl()));
+                        }
                     }
                 });
             }).start();
